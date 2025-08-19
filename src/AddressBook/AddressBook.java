7c957b5efb14f4e8ffc989eb014cbfc545e08218
Chetana NS <chetananaresh879@gmail.com>
2025-08-19 23:13:48 +0530

    Add Contact Page

---
+++ src/AddressBook/AddressBook.java
@@ -0,0 +1,19 @@
+package AddressBook;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<Contacts> contacts;
+
+    // Constructor
+    public AddressBook() {
+        contacts = new ArrayList<>();
+    }
+
+    // Add a new contact
+    public void addContact(Contacts contact) {
+        contacts.add(contact);
+        System.out.println("Contact added successfully!");
+    }
+}
